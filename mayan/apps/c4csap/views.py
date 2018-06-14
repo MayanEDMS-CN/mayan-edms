668a6fc0d567d995cdb1510ef9f0e55ef9d577cd
wang ying <wang.ying@sap.com>
2018-06-14 13:31:08 +0800

    no login required

--- mayan/apps/c4csap/views.py
+++ mayan/apps/c4csap/views.py
@@ -1,17 +1,44 @@
 from __future__ import absolute_import, unicode_literals
 
+from django import VERSION as DJANGO_VERSION
 from django.utils.translation import ugettext_lazy as _
 from django.views.generic import RedirectView, TemplateView
 import logging
 from django.template import RequestContext
-from stronghold.views import StrongholdPublicMixin
+from django.utils.decorators import method_decorator
+from stronghold.decorators import public
+from braces.views._access import AccessMixin
+from django.contrib.auth import authenticate, login
+
+if DJANGO_VERSION >= (1, 10, 0):
+    _is_authenticated = lambda user: user.is_authenticated  # noqa
+else:
+    # Django<1.10 compatibility
+    _is_authenticated = lambda user: user.is_authenticated()  # noqa
+
+class C4CSAPTokenLoginMixin(AccessMixin):
+
+    @method_decorator(public)
+    def dispatch(self, request, *args, **kwargs):
+        if not _is_authenticated(request.user):
+            c4c_username = request.GET.get("username", "")
+            c4c_token = request.GET.get("token", "")
+            if len(c4c_username)>0 and len(c4c_token)>0:
+                #user = authenticate(username=c4c_username, password=c4c_token)
+                user = authenticate(username="testkb", password="Welcome1")
+                if user is not None:
+                    login(request, user)
+                    return super(C4CSAPTokenLoginMixin, self).dispatch(*args, **kwargs)
+
+        return self.no_permissions_fail(request)
 
 
-class TabMashupView(StrongholdPublicMixin, TemplateView):
+
+class TabMashupView(C4CSAPTokenLoginMixin, TemplateView):
     template_name = 'appearance/home.html'
 
 
-class RelatedItemListView(StrongholdPublicMixin, TemplateView):
+class RelatedItemListView(C4CSAPTokenLoginMixin, TemplateView):
     template_name = 'c4csap/related_items.html'
 
     def get_context_data(self, **kwargs):
@@ -24,7 +51,7 @@ class RelatedItemListView(StrongholdPublicMixin, TemplateView):
         return data
 
 
-class RedirectToHomeView(StrongholdPublicMixin, RedirectView):
+class RedirectToHomeView(C4CSAPTokenLoginMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self, *args, **kwargs):
